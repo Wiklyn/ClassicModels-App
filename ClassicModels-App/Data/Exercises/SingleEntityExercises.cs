73c9cc2d4be6a1a1ffa0e5f3c8e3dac1be71bf9b
Wiklyn Craveiro <wiklynsc@gmail.com>
2025-01-06 02:39:37 -0300

    Adds exercise 1 to Single Entity Category

---
+++ ClassicModels-App/Data/Exercises/SingleEntityExercises.cs
@@ -0,0 +1,35 @@
+﻿using ClassicModels.Models;
+
+namespace ClassicModels.Data.Exercises
+{
+    public class SingleEntityExercises
+    {
+        public static readonly List<Exercise> ExerciseList = [
+            new (
+                1, "Prepare a list of offices sorted by country, state, city.",
+                () =>
+                {
+                    var context = new AppDbContext();
+
+                    return context.Offices
+                        .OrderBy(office => office.Country)
+                        .ThenBy(office => office.State)
+                        .ThenBy(office => office.City)
+                        .Select(office => new
+                        {
+                            office.OfficeCode,
+                            office.City,
+                            office.Phone,
+                            office.AddressLine1,
+                            office.AddressLine2,
+                            office.State,
+                            office.Country,
+                            office.PostalCode,
+                            office.Territory
+                        })
+                        .ToList();
+                }
+            ),
+        ];
+    }
+}
