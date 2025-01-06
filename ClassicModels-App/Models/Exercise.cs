51a4995da6cb4afa5bda64d3aabb9a89e9359b1a
Wiklyn Craveiro <wiklynsc@gmail.com>
2025-01-06 02:32:36 -0300

    Creates the Exercise model

---
+++ ClassicModels-App/Models/Exercise.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassicModels.Models
+{
+    public class Exercise(int Number, string Statement, Func<IEnumerable<object>> Query)
+    {
+        public int Number { get; } = Number;
+        public string Statement { get; } = Statement;
+        public Func<IEnumerable<object>> Query { get; } = Query;
+    }
+}
