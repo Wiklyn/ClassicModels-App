dd27a6c8e078f9f162b574320ff32527c11fdef1
Wiklyn Craveiro <wiklynsc@gmail.com>
2025-01-06 02:35:50 -0300

    Adds JSON Serializer

---
+++ ClassicModels-App/Utilities/Util.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ClassicModels.Utils
+{
+    public class Util
+    {
+        private static readonly JsonSerializerOptions Options = new()
+        {
+            WriteIndented = true,
+            DefaultIgnoreCondition = JsonIgnoreCondition.Never
+        };
+
+        public static void PrintAsJson<T>(T data)
+        {
+            var json = JsonSerializer.Serialize(data, Options);
+            Console.WriteLine();
+            Console.WriteLine(json);
+        }
+    }
+}
