8425483a19fc794c11ad46f079959f49d74eaf2f
Wiklyn Craveiro <wiklynsc@gmail.com>
2025-01-12 18:37:14 -0300

    Adds exercise 2 to One to Many Relationship Category

--- ClassicModels-App/Data/Exercises/OneToManyRelationshipExercises.cs
+++ ClassicModels-App/Data/Exercises/OneToManyRelationshipExercises.cs
@@ -1,4 +1,5 @@
 ﻿using ClassicModels.Models;
+using System.Globalization;
 
 namespace ClassicModels.Data.Exercises
 {
@@ -19,6 +20,23 @@ namespace ClassicModels.Data.Exercises
                             customer.CustomerName,
                             AccountRepNumber = customer.Employee.EmployeeNumber,
                             AccountRepName = customer.Employee.FirstName + " " + customer.Employee.LastName
+                        })
+                        .ToList();
+                }
+            ),
+            new (
+                2, "Report total payments for Atelier graphique.",
+                () =>
+                {
+                    var context = new AppDbContext();
+
+                    return context.Payments
+                        .Where(payment => payment.Customer.CustomerName == "Atelier graphique")
+                        .GroupBy(payment => payment.Customer.CustomerName)
+                        .Select(group => new
+                        {
+                            CustomerName = group.Key,
+                            TotalPayments = '$' + group.Sum(payment => payment.Amount).ToString("N2", new CultureInfo("en-US"))
                         })
                         .ToList();
                 }
