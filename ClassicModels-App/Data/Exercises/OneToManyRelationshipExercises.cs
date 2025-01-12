caf4614c5ce942bcc3f3e50630aa4e1e4439f207
Wiklyn Craveiro <wiklynsc@gmail.com>
2025-01-12 18:33:12 -0300

    Adds OneToManyRelationshipExercises class with exercise 1

---
+++ ClassicModels-App/Data/Exercises/OneToManyRelationshipExercises.cs
@@ -0,0 +1,28 @@
+﻿using ClassicModels.Models;
+
+namespace ClassicModels.Data.Exercises
+{
+    public class OneToManyRelationshipExercises
+    {
+        public static readonly List<Exercise> ExerciseList = [
+            new (
+                1, "Report the account representative for each customer.",
+                () =>
+                {
+                    var context = new AppDbContext();
+
+                    return context.Customers
+                        .Where(customer => customer.Employee != null)
+                        .Select(customer => new
+                        {
+                            customer.CustomerNumber,
+                            customer.CustomerName,
+                            AccountRepNumber = customer.Employee.EmployeeNumber,
+                            AccountRepName = customer.Employee.FirstName + " " + customer.Employee.LastName
+                        })
+                        .ToList();
+                }
+            ),
+        ];
+    }
+}
